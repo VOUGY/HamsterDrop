361497ed8db9d2bdab85ebf7f7b4d27b6f5d3ed5
Buubu <montaine@windowslive.com>
2018-05-26 11:55:01 +0200

    Gameplay layout

--- JS/Elements/canvas.js
+++ JS/Elements/canvas.js
@@ -4,6 +4,7 @@ var h = 600, w = 800,
     canvas = document.getElementById('c'), ctx = canvas.getContext('2d'), interval,
     line;
 
+
 function defineGameBox(){
     canvas.height = h;
     canvas.width = w;
@@ -12,16 +13,20 @@ function defineGameBox(){
     var l = (windowW - w)/2;
     var t = (windowH - h)/2;
 
+    document.getElementById("header").style.height = t.toString();
+    document.getElementById("footer").style.height = t.toString();
+    document.getElementById("left").style.width = l.toString();
+    document.getElementById("right").style.width = l.toString();
+
     canvas.style.position = "absolute";
     canvas.style.left = l.toString();
     canvas.style.top = t.toString();
     canvas.style.backgroundColor = "white";
 }
 
+
 function integrateObject(){
     // drawLines(listCalcLines);
     drawLines_rev(line);
     // drawRects(listCalcRect);
 }
-
-
