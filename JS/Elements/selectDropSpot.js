9e049e76ac8c0816e51503e9503af562d34d1b50
Nicolas Solioz <nicolas.solioz@students.hevs.ch>
2018-05-28 21:20:54 +0200

    NS - select drop spot now functional, returns position of mouse relative to the canvas
    Signed-off-by: Nicolas Solioz <nicolas.solioz@students.hevs.ch>

--- JS/Elements/selectDropSpot.js
+++ JS/Elements/selectDropSpot.js
@@ -1,17 +1,24 @@
-let ctx;
-let x = 100;
-let y = 100;
+var ctx;
+var x = 100;
+var y = 100;
 
-let dx = 5;
-let dy = 5;
+var dx = 5;
+var dy = 5;
 
-let canvas = document.getElementById("myCanvas");
+var clicked = false; //check that canvas hasn't been clicked yet
+
+var canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext('2d');
 
-window.onload = function() {
-    if (canvas.getContext)
-        let selectDrop = setInterval(selectDropSpot, 1500);
-	
+function  getMousePos(canvas, evt) { // source https://stackoverflow.com/questions/17130395/real-mouse-position-in-canvas
+    var rect = canvas.getBoundingClientRect(), // abs. size of element
+        scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
+        scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
+
+    return {
+        x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+        y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+    }
 }
 
 //redraw ball based on position of cursor
@@ -23,44 +30,52 @@ function selectDropSpot(e) {
 
 	ctx.fillStyle="#0000ff";
 
-	ctx.arc(e.clientX-400, y, 20,0,Math.PI*2,true);
+	var pos = getMousePos(canvas, e);
+
+	ctx.arc(pos.x, y, 20,0,Math.PI*2,true);
 
 	ctx.closePath();
 	ctx.fill();
 
 	//get x mouse position
-	x = e.clientX-400;
+	x = pos.x;
 
 }
-
-function ballDrop() {
+function ballDrop(pos) {
 
 	canvas.onmousemove = null; //cancel listener that selects drop spot
 
-	ctx.clearRect(0,0,440,550);
-	ctx.beginPath();
-	//define color of fill
-	ctx.fillStyle="#0000ff";
+	x = pos.x;
 
+	canvas.onclick = null;
 	//draw ball
-	ctx.arc(x,y,20,0,Math.PI*2,true);
+	setInterval(function() {
 
-	ctx.closePath();
-	ctx.fill();
+        ctx.clearRect(0,0,440,550);
+        ctx.beginPath();
+        //define color of fill
+        ctx.fillStyle="#0000ff";
 
-	if(x<0 || x>440)
-		dx = -dx;
+		ctx.arc(x,y,20,0,Math.PI*2,true);
 
-	if(y<0 || y>550)
-		dy = -dy;
+        ctx.closePath();
+        ctx.fill();
 
-	y+=dy;
+        if(x<0 || x>440)
+            dx = -dx;
+
+        if(y<0 || y>550)
+            dy = -dy;
+
+        y+=dy;
+
+    }, 5);
 
 }
 
-canvas.addEventListener('click', function(){  //on click, let the ball fall down
-
-	setInterval(ballDrop,5);
-	clearInterval(selectDrop);
-	
-}, false);
+canvas.addEventListener('click', function(e){  //on click, let the ball fall down
+	if (clicked == false) {
+	clicked = true;
+	ballDrop(getMousePos(canvas, e));
+	}
+}, true);
