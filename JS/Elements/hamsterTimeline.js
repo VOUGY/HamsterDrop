448438fb27cf1f1c62814be063ad45924016b614
Buubu <montaine@windowslive.com>
2018-06-07 10:51:00 +0200

    Add avatars and sliders to the game window

--- JS/Elements/hamsterTimeline.js
+++ JS/Elements/hamsterTimeline.js
@@ -1,22 +1,22 @@
 function hamsterTimeline(playerOneScore)
 {
     var i = 1;
-    document.getElementById("hamsterOne").disabled = true;
-    document.getElementById("hamsterTwo").disabled = true;
-    document.getElementById("hamsterOne").value = i;
-    document.getElementById("hamsterTwo").value = i;
+    document.getElementById("NS_hamsterOne").disabled = true;
+    document.getElementById("NS_hamsterTwo").disabled = true;
+    document.getElementById("NS_hamsterOne").value = i;
+    document.getElementById("NS_hamsterTwo").value = i;
 
     setInterval(function (){
         do {
             if(i < playerOneScore)
-                document.getElementById("hamsterOne").value = i;
+                document.getElementById("NS_hamsterOne").value = i;
             else
             {
                 document.getElementById("resultField").innerHTML = "PLAYER ONE FINISHED !";
-                document.getElementById("hamsterOne").style.background = "#92f442";
+                document.getElementById("NS_hamsterOne").style.background = "#92f442";
             }
 
-            document.getElementById("hamsterTwo").value = i++;
+            document.getElementById("NS_hamsterTwo").value = i++;
         }while(i>24000);
     }, 5);
 }
