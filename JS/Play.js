b6acc896e041ee74a38c922ee273ebb4a6bd42fa
VOUGY <guillaume.vouilloz@outlook.com>
2018-06-09 00:13:25 +0200

    Ameliorate classes
    Création d'une classe win qui remplace win mais ne fonctionne pas bien
    Correction dans play et player (intégration du score)

--- JS/Play.js
+++ JS/Play.js
@@ -5,14 +5,13 @@ class Play {
         this.iCurrentUser = 0;
         this.aPlayers = [];
         this.location = "";
-
     }
     reload(json)
     {
         this.iTotalPlayer=json.iTotalPlayer;
         json.aPlayers.forEach(function(element)
         {
-           Game.addPlayer(element.name,element.idhamster)
+           Game.reloadPlayer(element.playerId,element.name,element.idhamster,element.score)
         })
         //  this.aPlayers = json.aPlayers;
         this.location = json.location;
@@ -26,13 +25,26 @@ class Play {
     {
         console.log("Création d'un nouveau joueur")
 
-            var playertemp = new Player(this.iCurrentUser,name, idHamster);
+            var playertemp = new Player(this.iCurrentUser,name, idHamster,0);
             this.aPlayers[this.iCurrentUser] = playertemp;
 
             console.log("Le joueur " + (this.iCurrentUser + 1) + "/" + (this.iTotalPlayer) + "vient d'etre ajouter");
             console.log(this.aPlayers[this.iCurrentUser].name + "  " + idHamster)
 
             this.iCurrentUser++;
+
+    }
+    reloadPlayer(id,name, idHamster,score)
+    {
+        console.log("Création d'un nouveau joueur")
+
+        var playertemp = new Player(id,name, idHamster,score);
+        this.aPlayers[this.iCurrentUser] = playertemp;
+
+        console.log("Le joueur " + (this.iCurrentUser + 1) + "/" + (this.iTotalPlayer) + "vient d'etre ajouter");
+        console.log(this.aPlayers[this.iCurrentUser].name + "  " + idHamster)
+
+        this.iCurrentUser++;
 
     }
     setiTotalPlayer(totalPlayer)
@@ -64,77 +76,3 @@ class Play {
 
 
 }
-/*
-class Tournament
-{
-    constructor(){
-        this.tournament = [];
-        this.nbrMatch = aPlayers.length/2;
-        console.log(this.nbrMatch);
-    }
-
-    startTournamen(){
-        for(i = 0; i<aPlayers.length; i+2)
-        {
-            addMatch(player[i], player[i+1])
-            y = y+2;
-        }
-    }
-
-    addMatch(player1,player2)
-    {
-        this.tournament.push(new Match(player1,player2));
-    }
-
-    winTournament()
-    {
-
-    }
-
-}
-/*
-class Player {
-    constructor(id,name,idhamster)
-    {
-        this.playerId = id;
-        this.name = name;
-        this.idhamster = idhamster;
-        this.score = [];
-        this.active = true;
-
-        console.log("A new player " + this.name)
-    }
-
-    addscore(level,value, time) {
-        this.score[level].value = value;
-        this.score[level].time = time;
-        console.log("score of player " + this.name + " :" + this.score[level].value + " during the level " + level)
-    }
-    score()
-    {
-        return this.score.time;
-    }
-}*/
-/*
-class Match {
-    constructor(player1, player2) {
-        this.player1 = Player(player1);
-        this.player2 = Player(player2);
-        this.end = false;
-    }
-
-    win() {
-        if (this.player1.score() > this.player2.score()) {
-            return this.player1;
-        } else {
-            return this.player2;
-        }
-        this.end = true;
-    }
-
-    getend()
-    {
-        return this.end;
-    }
-}*/
-
