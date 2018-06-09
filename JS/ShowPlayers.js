c65cafd3311e9cdac6afb643ee1e19effe4acad3
Buubu <montaine@windowslive.com>
2018-06-09 02:26:53 +0200

    Show the losers in a different way + begin to code the matches' display

--- JS/ShowPlayers.js
+++ JS/ShowPlayers.js
@@ -10,8 +10,14 @@ for (var player = 0; player < table.length; player++) {
     var ham = hamster.substr(0, 4);
     var avatar = ham + ".png";
 
-    html += '<div class="container" style="margin-top: 20px"><img style="height: 45px; width: 47px" src="IMAGE/Avatars/'
-        + avatar + '"/><text style="margin-left: 10px">' + name + '</text></div>';
+    if (table[player].active === true) {
+        html += '<div class="container" style="margin-top: 20px"><img style="height: 45px; width: 47px" src="IMAGE/Avatars/'
+            + avatar + '"/><text style="margin-left: 10px">' + name + '</text></div>';
+    }
+    else {
+        html += '<div class="container" style="margin-top: 20px"><img style="height: 45px; width: 47px; filter: grayscale(100%)" src="IMAGE/Avatars/'
+            + avatar + '"/><text style="margin-left: 10px; text-decoration: line-through">' + name + '</text></div>';
+    }
 }
 
 div.innerHTML = html;
